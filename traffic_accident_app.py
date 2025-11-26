2b6dc86580af93497e9b4196ef834229b9e662b3
choihyeongbae <149979597+choihyeongbae@users.noreply.github.com>
2025-11-26 19:37:32 +0900

    Update traffic_accident_app.py

--- traffic_accident_app.py
+++ traffic_accident_app.py
@@ -9,7 +9,7 @@ from sklearn.metrics import silhouette_score
 from io import StringIO
 
 # 한글 폰트 설정
-rc.('font', family='Malgun Gothic')
+rc('font', family='Malgun Gothic')
 plt.rcParams['axes.unicode_minus'] = False
 
 # 내장 데이터 (2024년 전국 지역별 어린이 교통사고 데이터)

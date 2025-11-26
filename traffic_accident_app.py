cd3b4a3f88f8c785ac99aa3e29cad7521e22e1c7
choihyeongbae <149979597+choihyeongbae@users.noreply.github.com>
2025-11-26 19:46:31 +0900

    Update traffic_accident_app.py

--- traffic_accident_app.py
+++ traffic_accident_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib import rc, fontManager
+from matplotlib import rc, font_manager
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import silhouette_score
